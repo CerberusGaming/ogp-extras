1faec879f43699d7721cc8ffb00de32d646503a2
Derek Vance <DACRepair@gmail.com>
2018-09-09 10:21:06 -0400

    Added run.sh handler

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ENV Vars
+# $EXTRAS_FOLDER - folders where the extras are downloaded to
+# $APACHE_DOCUMENT_ROOT - the apache document root (where OGP is installed by default)
+
+# YOU ARE IN CHARGE OF CLEANING UP YOUR ARTIFACTS
+
+#install obsidian theme
+git clone https://github.com/hmrserver/Obsidian.git /tmp/obsidian
+mv /tmp/obsidian/themes $EXTRAS_FOLDER
+rm -rf /tmp/obsidian
